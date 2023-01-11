b7695cc13b25e5cf6304db3b7ab1469b46ef28a2
dixk3458 <dixk3458@naver.com>
2023-01-11 18:43:26 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,9 +9,9 @@ undefined;
 
 const gameFinishBanner = new PopUp();
 const game = new GameBuilder()
-  .withGameDuration(5)
-  .withCarrotCount(3)
-  .withBugCount(3)
+  .withGameDuration(20)
+  .withCarrotCount(20)
+  .withBugCount(20)
   .build();
 
 game.setGameStopListener(reason => {
